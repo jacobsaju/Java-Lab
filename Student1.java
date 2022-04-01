66cbd570ca8ab74f4513b781764a2036d7d257fc
jacobsaju <95347616+jacobsaju@users.noreply.github.com>
2022-04-01 14:03:41 +0530

    Add files via upload

---
+++ Student1.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package student1;
+
+/**
+ *
+ * @author sjcet
+ */
+public class Student1 {int m1,m2,m3;
+public Student1(int a, int b, int c) {
+this.m1 = a;
+this.m2 = b;
+this.m3 = c;
+}
+public void print()
+{
+System.out.println("mark of first subject "+m1);
+System.out.println("mark of second subject "+m2);
+System.out.println("mark of third subject "+m3);
+}
+public void sum()
+{
+int sum=m1+m2+m3;
+System.out.println("sum is"+sum);
+}
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Student1 s1=new Student1(10,20,30);
+        Student1 s2=new Student1(50,40,30);
+        s1.print();
+        s1.sum();
+        s2.print();
+        s2.sum();
+        
+    
+        // TODO code application logic here
+    }
+    
+}
